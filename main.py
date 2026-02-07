a01118e8ba544a9d23dfc84a466d4f39b836b5cc
user <user@email.com>
2026-02-07 15:57:52 -0500

    Refactor GPX processing functions

--- main.py
+++ main.py
@@ -1,31 +1,5 @@
 import parser
-import trackPoint
 
-parser.read_GPX('20260205.gpx')
-parser.process_GPX('output.gpx')
-
-list = parser.process_GPX('output.gpx')
+list = parser.get_GPX('20250612_Morning.gpx')
 
 
-import matplotlib.pyplot as plt
-
-lats = [p.lat for p in list]
-lons = [p.lon for p in list]
-alt = [p.geoidheight for p in list]
-speeds = [p.speed for p in list]
-
-plt.figure(figsize=(8, 8))
-plt.plot(lons, lats, "-o", markersize=2)
-plt.xlabel("Longitude")
-plt.ylabel("Latitude")
-plt.title("GPS Track")
-plt.axis("equal")
-plt.grid(True)
-plt.show()
-
-plt.figure(figsize=(8, 8))
-plt.scatter(lons, lats, c=speeds, s=5, cmap="viridis")
-plt.colorbar(label="Speed (m/s)")
-plt.axis("equal")
-plt.title("Track Colored by Speed")
-plt.show()
--- parser.py
+++ parser.py
@@ -3,51 +3,40 @@ import xml.etree.ElementTree as ET
 from datetime import datetime
 from trackPoint import trackPoint
 
-def read_GPX(filename):
+def getGPX(filename):
     try:
-        with open(filename, 'r') as file:
-            gpx_file = open(filename)
-            gpx = gpxpy.parse(gpx_file)
-            xml = gpx.to_xml()
-        with open("output.gpx", 'w') as f:
-            f.write(xml)
+        with open(filename, "r", encoding="utf-8") as f:
+            gpx = gpxpy.parse(f)
             
     except FileNotFoundError:
         print(f"File '{filename}' was not found.")
+        return []
 
-def process_GPX(filename):
     ns = {"gpx": "http://www.topografix.com/GPX/1/0"}
-    
+
     tree = ET.parse(filename)
     root = tree.getroot()
 
     trackPoints = []
 
     for trkpt in root.findall(".//gpx:trkpt", ns):
-            data = {
-                "lat": float(trkpt.attrib["lat"]),
-                "lon": float(trkpt.attrib["lon"]),
-            }
+        data = {
+            "lat": float(trkpt.attrib["lat"]),
+            "lon": float(trkpt.attrib["lon"]),
+        }
 
-            for child in trkpt:
-                tag = child.tag.split("}")[-1]
-                text = child.text
+        for child in trkpt:
+            tag = child.tag.split("}")[-1]
+            text = child.text
 
-                if tag == "time":
-                    data[tag] = datetime.fromisoformat(text.replace("Z", "+00:00"))
-                elif tag in {"ele", "course", "speed", "geoidheight", "hdop", "vdop", "pdop"}:
-                    data[tag] = float(text)
-                elif tag == "sat":
-                    data[tag] = int(text)
-                else:
-                    data[tag] = text
+            if tag == "time":
+                data[tag] = datetime.fromisoformat(text.replace("Z", "+00:00"))
+            elif tag in {"ele", "course", "speed", "geoidheight", "hdop", "vdop", "pdop"}:
+                data[tag] = float(text)
+            elif tag == "sat":
+                data[tag] = int(text)
+            else:
+                data[tag] = text
 
-            trackPoints.append(trackPoint(**data))
+        trackPoints.append(trackPoint(**data))
     return trackPoints
-
-
-
-
-
-
-
