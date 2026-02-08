e586f779379cec298e1e7954e762531d93a6a614
user <user@email.com>
2026-02-08 10:17:21 -0500

    added back the graphs

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 import parser
 
-list = parser.get_GPX('20250612_Morning.gpx')
+list = parser.getGPX('20250612_Morning.gpx')
 
+import matplotlib.pyplot as plt
+
+lats = [p.lat for p in list]
+lons = [p.lon for p in list]
+alt = [p.geoidheight for p in list]
+speeds = [p.speed for p in list]
+
+plt.figure(figsize=(8, 8))
+plt.plot(lons, lats, "-o", markersize=2)
+plt.xlabel("Longitude")
+plt.ylabel("Latitude")
+plt.title("GPS Track")
+plt.axis("equal")
+plt.grid(True)
+plt.show()
+
+plt.figure(figsize=(8, 8))
+plt.scatter(lons, lats, c=speeds, s=5, cmap="viridis")
+plt.colorbar(label="Speed (m/s)")
+plt.axis("equal")
+plt.title("Track Colored by Speed")
+plt.show()
 
