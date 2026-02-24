4d7472e6e5bb29383dcf6bbb3d3150b06842daa6
user <user@email.com>
2026-02-24 15:36:20 -0500

    Add debug for DB func

---
+++ debugDBfunc.py
@@ -0,0 +1,9 @@
+from backend import databaseFunctions
+
+
+
+test = databaseFunctions.calculate_elevation_stats(1)
+test2 = databaseFunctions.calculate_elevation_stats(4)
+
+print(test)
+print(test2)
